3e00b720960cfd55dff70f2832e2ffe3a9b4f8e4
Osman Kara <osmankara@outlook.com>
2019-12-08 23:59:55 -0700

    Initial commit

---
+++ YourCompany.MicroserviceTemplate/Controllers/MyController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.ApplicationInsights;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace YourCompany.MicroserviceTemplate.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class MyController : ControllerBase
+    {
+
+        private readonly ILogger<MyController> _logger;
+        private readonly TelemetryClient _telemetryClient;
+
+        public MyController(ILogger<MyController> logger, TelemetryClient telemetryClient)
+        {
+            _logger = logger;
+            _telemetryClient = telemetryClient;
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            _logger.LogInformation("Get called!");
+            _telemetryClient.TrackEvent("Sample event if you want", null, null);
+
+            return Ok("Get successful!");
+        }
+
+        [HttpPost]
+        public IActionResult Post()
+        {
+            return Created("Sample uri", "Post successful!");
+        }
+    }
+}
---
+++ YourCompany.MoreBusinessLogicProjects/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace YourCompany.MoreBusinessLogicProjects
+{
+    public class Class1
+    {
+    }
+}
